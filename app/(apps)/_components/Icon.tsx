fa3a12198a2242b09d40162ae201218332be3472
howlil <muhammadulilabshar27@gmail.com>
2024-05-08 09:39:55 +0700

    change lang

--- app/(apps)/_components/Icon.tsx
+++
@@ -1,9 +0,0 @@
-import { icons } from 'lucide-react';
-
-const Icon = ({ name, color, size }:any) => {
-  const LucideIcon = icons[name];
-
-  return <LucideIcon color={color} size={size} />;
-};
-
-export default Icon;
--- app/(apps)/dashboard/page.tsx
+++
@@ -1,4 +0,0 @@
-const App = () => {
-  return <div className="mt-[80px] ml-[16px]"> </div>;
-};
-export default App;
